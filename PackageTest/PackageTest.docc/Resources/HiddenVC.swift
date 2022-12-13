1208bd914c19838751a35629583014ad023bf066
reshmyunni <reshmyunni1778@gmail.com>
2022-12-13 16:59:14 +0530

    Initial Commit

---
+++ PackageTest/PackageTest.docc/Resources/HiddenVC.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//
+//  Created by Reshmy Manuraj on 13/12/22.
+//
+
+import Foundation
+import UIKit
+
+class HiddevVC:UIViewController{
+    override func viewDidLoad(){
+        super.viewDidLoad()
+    }
+    public func startFramework(){
+        print("Framework Started")
+    }
+}
---
+++ PackageTest/PackageTest.h
@@ -0,0 +1,18 @@
+//
+//  PackageTest.h
+//  PackageTest
+//
+//  Created by Reshmy Manuraj on 13/12/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PackageTest.
+FOUNDATION_EXPORT double PackageTestVersionNumber;
+
+//! Project version string for PackageTest.
+FOUNDATION_EXPORT const unsigned char PackageTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PackageTest/PublicHeader.h>
+
+
